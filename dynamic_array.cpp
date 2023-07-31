ec507397c3eb46a6eafd367b76d84aed7c4953eb
abhidhole2002 <134376307+abhidhole2002@users.noreply.github.com>
2023-07-31 21:52:05 +0530

    Add files via upload

---
+++ dynamic_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int *arr = new int[10];
+
+    cout<<"enter 10 elements : ";
+    for(int i=0;i<10;i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"array elements : ";
+    for(int i=0;i<10;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl<<"another method : ";
+    for(int i=0;i<10;i++)
+    {
+        cout<<*(arr+i)<<" ";
+    }
+ 
+ 
+}
